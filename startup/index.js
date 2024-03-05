44f6c5e8fd7dd23478da29caeb9e617027050a4d
kobydj <kobyjensen@gmail.com>
2024-03-05 13:11:30 -0700

    added express and put on port 4000 fixed index.js

--- startup/index.js
+++ startup/index.js
@@ -1,10 +1,35 @@
-const http = require('http');
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<h1>Hello Node.js! [${req.method}] ${req.url}</h1>`);
-  res.end();
+const express = require('express');
+const app = express();
+
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// GetScores
+apiRouter.get('/scores', (_req, res) => {
+  res.send(scores);
 });
 
-server.listen(8080, () => {
-  console.log(`Web service listening on port 8080`);
+// SubmitScore
+apiRouter.post('/score', (req, res) => {
+  scores = updateScores(req.body, scores);
+  res.send(scores);
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
