9e120c05f4b955a47cf89fda9c7b66129cae7b1d
kobydj <kobyjensen@gmail.com>
2024-03-19 13:35:28 -0600

    setting up for login stuff

--- public/login.js
+++ public/login.js
@@ -1,6 +1,37 @@
+(async () => {
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    const usernameEl = document.querySelector('#userName');
+    usernameEl.textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('postLogin', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('postLogin', 'none');
+  }
+})();
+
+function setDisplay(controlId, display) {
+  const loginControlEl = document.querySelector(`#${controlId}`);
+  if (loginControlEl) {
+    loginControlEl.style.display = display;
+  }
+}
+
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "Garden.html";
+  }
+
+function create() {
+    const nameEl = document.querySelector("#name");
+    localStorage.setItem("userName", nameEl.value);
+    window.location.href = "Garden.html";
+  }
+
+function create() {
+  localStorage.removeItem('userName');
+
   }
   
