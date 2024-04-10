82756b699674b1c2c9d5f643a7e1de1f45397dc5
kobydj <kobyjensen@gmail.com>
2024-04-09 20:47:30 -0600

    added sections for each component

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,68 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Garden } from './garden/garden';
+import { Info } from './info/info';
+import { About } from './about/about';
+import { AuthState } from './login/authState';
 
-export default function App() {
+
+function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+
   return (
+    <BrowserRouter>
     <div className= "body text-light">
-        <header class="container-fluid">
-            <nav class= "navbar fixed-top navbar-dark">
-            <a  class="navbar-brand">
+        <header className="container-fluid">
+            <nav className= "navbar fixed-top navbar-dark">
+            <a  className="navbar-brand">
               <h1>Gardentimer</h1>
             </a>
-            <menu class="navbar-nav" >
-              <li class="nav-item">
-                <a class="nav-link active" href="index.html" >Home</a></li>
-              <li class="nav-item">
-                <a class="nav-link" href="garden.html">Garden</a></li>
-              <li class="nav-item">
-                <a class="nav-link" href="info.html">Plant Info</a></li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.html">About</a></li>
+            <menu className="navbar-nav" >
+              <li className="nav-item">
+              <NavLink className='nav-link' to=''>Login</NavLink></li>
+              <li className="nav-item">
+              <NavLink className='nav-link' to='garden'>Garden</NavLink></li>
+              <li className="nav-item">
+              <NavLink className='nav-link' to='info'>Plant Info</NavLink></li>
+              <li className="nav-item">
+              <NavLink className='nav-link' to='about'>About</NavLink></li>
             </menu>
           </nav>
         </header>
-        <main>App components go here</main>
-        <footer class="text-white-50">
-          <div class="container-fluid">
-            <span class="text-reset">Koby Jensen</span>
-            <a class="text-reset" href="https://github.com/kobydj/startup.git">GitHub</a>
+        <Routes>
+          <Route path='/' element={<Login 
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+            exact
+          />
+          <Route path='/garden' element={<Garden />} />
+          <Route path='/info' element={<Info />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <footer className="text-white-50">
+          <div className="container-fluid">
+            <span className="text-reset">Koby Jensen</span>
+            <a className="text-reset" href="https://github.com/kobydj/startup.git">GitHub</a>
           </div>
         </footer>  
     </div>
+    </BrowserRouter>
+
     );
+    function NotFound() {
+      return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+    }
 }
---
+++ src/garden/garden.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Garden() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>garden displayed here</div>
+    </main>
+  );
+}
