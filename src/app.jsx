2a0a028c550d6e9da27f240aa52ab0809630d0b1
kobydj <kobyjensen@gmail.com>
2024-04-09 19:54:45 -0600

    added stuff

--- src/app.jsx
+++ src/app.jsx
@@ -7,28 +7,28 @@ export default function App() {
     <div className= "body text-light">
         <header class="container-fluid">
             <nav class= "navbar fixed-top navbar-dark">
-        <a  class="navbar-brand">
-          <h1>Gardentimer</h1>
-        </a>
-        <menu class="navbar-nav" >
-          <li class="nav-item">
-            <a class="nav-link active" href="index.html" >Home</a></li>
-          <li class="nav-item">
-            <a class="nav-link" href="garden.html">Garden</a></li>
-          <li class="nav-item">
-            <a class="nav-link" href="info.html">Plant Info</a></li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.html">About</a></li>
-        </menu>
-      </nav>
-    </header>
-    <main>App components go here</main>
-    <footer class="text-white-50">
-      <div class="container-fluid"></div>
-        <span class="text-reset">Koby Jensen</span>
-        <a class="text-reset" href="https://github.com/kobydj/startup.git">GitHub</a>
-      </div>
-    </footer>  
+            <a  class="navbar-brand">
+              <h1>Gardentimer</h1>
+            </a>
+            <menu class="navbar-nav" >
+              <li class="nav-item">
+                <a class="nav-link active" href="index.html" >Home</a></li>
+              <li class="nav-item">
+                <a class="nav-link" href="garden.html">Garden</a></li>
+              <li class="nav-item">
+                <a class="nav-link" href="info.html">Plant Info</a></li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a></li>
+            </menu>
+          </nav>
+        </header>
+        <main>App components go here</main>
+        <footer class="text-white-50">
+          <div class="container-fluid">
+            <span class="text-reset">Koby Jensen</span>
+            <a class="text-reset" href="https://github.com/kobydj/startup.git">GitHub</a>
+          </div>
+        </footer>  
     </div>
     );
 }
