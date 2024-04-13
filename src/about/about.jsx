14ac828f43e594f91b66fd4493dd11df298b6a2b
kobydj <kobyjensen@gmail.com>
2024-04-12 19:27:35 -0600

    got the images to deploy and show up

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -5,7 +5,7 @@ export function About() {
   return (
     <main className="main container-fluid text-dark text-center">
 
-    <div className="picture-box" id="picture" ><img width="400px" src="/assets/harvest picture.jpg" alt="harvest picture" /></div>
+    <div className="picture-box" id="picture" ><img width="400px" src="/harvest picture.jpg" alt="harvest picture" /></div>
 
     <p className="description">
       Gardentimer allows you to keep track of when you planted things as well as helpful information for each plant
